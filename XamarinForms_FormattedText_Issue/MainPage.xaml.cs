e5571edd9c9fa821cfed651e56592be5e5755b6f
Robert Haßfeld <rhassfeld@h3ko.de>
2021-04-19 10:16:27 +0200

    add new xamarin forms project with a example page

---
+++ XamarinForms_FormattedText_Issue/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace XamarinForms_FormattedText_Issue
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Debug.WriteLine(sender.ToString());
+            Debug.WriteLine(e.ToString());
+        }
+    }
+}
+
